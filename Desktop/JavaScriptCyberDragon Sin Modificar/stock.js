917e97004e154b15697b5c1c83d89feb07cf53eb
Ingagonzalo <103911185+Ingagonzalo@users.noreply.github.com>
2022-10-23 19:30:44 -0300

    Delete Desktop directory

--- Desktop/JavaScriptCyberDragon Sin Modificar/stock.js
+++
@@ -1,8 +0,0 @@
-let stockProductos = [
-    {id: 1, img : '../images/discorigido1.jpg', nombre: "Disco Rigido Hdd 1tb Western Digital Blue", categoria: "almacenamiento", cantidad: 1, precio: 5989},
-    {id: 2, img : '../images/discorigido2.jpg', nombre: "Disco Rigido Hdd 1tb Seagate Barracuda", categoria: "almacenamiento", cantidad: 1, precio: 6454},
-    {id: 3, img : '../images/discorigido3.jpg', nombre: "Disco Rigido Hdd 2tb Western Digital Blue 256mb", categoria: "almacenamiento", cantidad: 1, precio: 6974},
-    {id: 4, img : '../images/discorigido4.jpg', nombre: "Disco Rigido Hdd 6tb Toshiba Performance X300", categoria: "almacenamiento", cantidad: 1, precio: 25634},
-    
-
-]
