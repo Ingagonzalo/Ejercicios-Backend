00e8f3b68e7811319293ac30055287344c3a987f
Gonzalo Inga <103911185+Ingagonzalo@users.noreply.github.com>
2022-12-06 19:45:24 -0300

    Add files via upload

---
+++ Motores de Plantillas/ejs/src/server.js
@@ -0,0 +1,44 @@
+debugger
+
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+const ProductosContainer = require('../productos.js');
+const productos = new ProductosContainer();
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+let form = Boolean(true);
+
+app.get('/', (req, res) => {
+  form = true;
+  res.render('../views/main', { form });
+})
+
+app.get('/productos', async (req, res) => {
+  form = false;
+  const getProductos = await productos.getAll();
+  res.render('../views/main', { getProductos, form });
+})
+
+app.post('/productos', async (req, res) => {
+  form = false;
+  const { nombre, precio, url } = req.body;
+  await productos.save({ nombre, precio, url });
+  const getProductos = await productos.getAll();
+  res.render('../views/main', { getProductos, form });
+})
+
+const port = 8080;
+
+const server = app.listen(port, () => {
+  console.log(`Servidor http escuchando en el puerto http://localhost:${port}`);
+});
+
+server.on('error', error => {
+  console.log('Error en servidor', error);
+});
