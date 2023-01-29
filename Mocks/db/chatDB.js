bf21895760f196e478ad7160f742e05368c81a0a
Gonzalo Inga <103911185+Ingagonzalo@users.noreply.github.com>
2023-01-29 12:39:43 -0300

    Delete Mocks directory

--- Mocks/db/chatDB.js
+++
@@ -1,7 +0,0 @@
-export const chatOptions = {
-    client: 'sqlite3',
-    connection: {
-        filename: "./db/chatdb.sqlite"
-    },
-    useNullAsDefault: true
-}
--- Mocks/db/productDB.js
+++
@@ -1,9 +0,0 @@
-export const productsOptions = {
-    client: 'mysql',
-    connection: {
-        host: process.env.HOST || 'localhost',
-        user: process.env.USER || 'root',
-        password: process.env.PASSWORD || '',
-        database: process.env.DATABASE || 'coder',
-    }
-}
